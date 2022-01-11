3147fa69a0d62b5dd6a1b3922243d4e1572a1e48
Urs Stutz <git@ls3d.de>
2022-01-11 17:10:04 +0100

    change email address

--- dist/sendit.php
+++ dist/sendit.php
@@ -4,7 +4,7 @@ $email = $_POST['email'];
 $subject = $_POST['subject'];
 $message = $_POST['message'];
 $formcontent="Von: $name\nEmail: $email\nBetreff: $subject\nNachricht: $message";
-$recipient = "test@steinkress.ls3d.de";
+$recipient = "info@steinkress.com";
 $mailheader = "From: $email\r\n";
 
 $response = mail($recipient, $subject, $formcontent, $mailheader) or die("error");
